942574f1b630ffc129c0f8b8eb2377c7ece45958
Bobby Earl <bobby.earl@blackbaud.com>
2016-09-28 12:57:36 -0400

    Writing metadata.json

--- config/webpack/build.webpack.config.js
+++ config/webpack/build.webpack.config.js
@@ -3,18 +3,68 @@
 
 const fs = require('fs');
 const path = require('path');
+const util = require('util');
 const webpack = require('webpack');
 const webpackMerge = require('webpack-merge');
 const ChunkManifestPlugin = require('chunk-manifest-webpack-plugin');
 
 /**
- * Saves the stats.json file (needed for entry order)
+ * Writes a json object to the dist folder.
+ * @name writeFileToDist
+ * @param {String} name
+ * @param {String} json
+ */
+const writeJson = (name, json) => {
+  fs.writeFileSync(
+    path.join(process.cwd(), 'dist', name),
+    JSON.stringify(json, null, '\t')
+  );
+};
+
+/**
+ * Saves the stats.json file
  * @name SaveStats
  */
 const SaveStats = function () {
   this.plugin('done', (stats) => {
-    const json = JSON.stringify(stats.toJson());
-    fs.writeFileSync(path.join(process.cwd(), 'dist', 'stats.json'), json);
+    writeJson('stats.json', stats.toJson());
+  });
+};
+
+/**
+ * Saves the metadata.json file.
+ * Used to store order, fallback variable, etc.
+ * @name SaveMetadata
+ */
+const SaveMetadata = function () {
+  this.plugin('emit', (compilation, done) => {
+    const formatName = 'SKYUX_READY_%s';
+    const formatDeclare = '%s\nvar %s = true;\n';
+    const metadata = [];
+
+    // Only care about JS files
+    Object.keys(compilation.assets).forEach((key) => {
+      const parsed = path.parse(key);
+
+      console.log(parsed);
+      if (parsed.ext !== '.js') {
+        return;
+      }
+
+      const asset = compilation.assets[key];
+      const source = asset.source();
+      const fallback = util.format(formatName, parsed.name.toUpperCase());
+
+      // Add our variable to the bottom of the source file
+      asset.source = () => util.format(formatDeclare, source, fallback);
+      metadata.push({
+        name: key,
+        fallback: fallback
+      });
+    });
+
+    writeJson('metadata.json', metadata);
+    done();
   });
 };
 
@@ -33,10 +83,11 @@ const getWebpackConfig = (skyPagesConfig) => {
     devtool: 'source-map',
     plugins: [
       SaveStats,
+      SaveMetadata,
       new webpack.optimize.DedupePlugin(),
       new ChunkManifestPlugin({
-        filename: "manifest.json",
-        manifestVariable: "webpackManifest"
+        filename: 'manifest.json',
+        manifestVariable: 'webpackManifest'
       }),
       new webpack.optimize.UglifyJsPlugin({
         beautify: false,
