5b67bf7d9178bcd4861a1ea165b0cdc4924fb249
Bobby Earl <bobby.earl@blackbaud.com>
2016-12-08 15:58:52 -0500

    Adjusted exported class regex + tests

--- test/sky-pages-component-generator.spec.js
+++ test/sky-pages-component-generator.spec.js
@@ -14,7 +14,7 @@ describe('SKY Pages component generator', () => {
     const file = 'me.component.ts';
 
     spyOn(glob, 'sync').and.returnValue([file]);
-    spyOn(fs, 'readFileSync').and.returnValue('class MyCustomComponent');
+    spyOn(fs, 'readFileSync').and.returnValue('@Component({}) export class MyCustomComponent {}');
 
     const components = generator.getComponents({
       srcPath: '',
@@ -23,15 +23,79 @@ describe('SKY Pages component generator', () => {
     expect(components.names).toContain('MyCustomComponent');
   });
 
-  it('should throw an error if a component name could not be extracted', () => {
+  it('should extract a component name regardless of whitespace', () => {
     const fs = require('fs');
     const glob = require('glob');
-    const file = 'my-component.component.ts';
+    const file = 'me.component.ts';
 
     spyOn(glob, 'sync').and.returnValue([file]);
-    spyOn(fs, 'readFileSync').and.returnValue('file without a class');
+    spyOn(fs, 'readFileSync').and.returnValue(`
+      @Component({
+
+      })
+      export     class      MyCustomComponent4 {
+
+      }  `);
+
+    const components = generator.getComponents({
+      srcPath: '',
+      componentsPattern: ''
+    });
+    expect(components.names).toContain('MyCustomComponent4');
+  });
+
+  it('should throw an error if an exported class could not be found', () => {
+    const fs = require('fs');
+    const glob = require('glob');
+    const file = 'my-component1.component.ts';
+
+    spyOn(glob, 'sync').and.returnValue([file]);
+    spyOn(fs, 'readFileSync').and.returnValue('class MyClass {}');
 
     const err = new Error(`Unable to locate an exported class in ${file}`);
+    const wrapper = function () {
+      generator.getComponents({
+        srcPath: '',
+        componentsPattern: ''
+      });
+    };
+
+    expect(wrapper).toThrow(err);
+  });
+
+  it('should throw an error if the unable to find the @Component decorator', () => {
+    const fs = require('fs');
+    const glob = require('glob');
+    const file = 'my-component2.component.ts';
+
+    spyOn(glob, 'sync').and.returnValue([file]);
+    spyOn(fs, 'readFileSync').and.returnValue('export class MyClass {}');
+
+    const err = new Error(`Unable to locate an exported class in ${file}`);
+    const wrapper = function () {
+      generator.getComponents({
+        srcPath: '',
+        componentsPattern: ''
+      });
+    };
+
+    expect(wrapper).toThrow(err);
+  });
+
+  it('should throw an error if more than one exported component per file', () => {
+    const fs = require('fs');
+    const glob = require('glob');
+    const file = 'my-component3.component.ts';
+
+    spyOn(glob, 'sync').and.returnValue([file]);
+    spyOn(fs, 'readFileSync').and.returnValue(`
+      @Component({
+      }) export class MyCustomComponent1 {}
+
+      @Component({
+      }) export class MyCustomComponent2 {}`);
+
+    const err = new Error(`As a best practice, please export one component per file in ${file}`);
     const wrapper = function () {
       generator.getComponents({
         srcPath: '',
@@ -64,7 +128,7 @@ describe('SKY Pages component generator', () => {
     const file = 'me.component.ts';
 
     spyOn(glob, 'sync').and.returnValue([file]);
-    spyOn(fs, 'readFileSync').and.returnValue('class MyComponent');
+    spyOn(fs, 'readFileSync').and.returnValue('@Component({}) export class MyComponent {}');
 
     const components = generator.getComponents({
       srcPath: '',
