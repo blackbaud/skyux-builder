f627248e1d58467fad5f9c2a2c6861e922385b5d
Paul Crowder <paul.crowder@blackbaud.com>
2017-01-27 13:09:09 -0500

    Import bootstrapper from node_modules (#62)

--- src/main-internal.aot.ts
+++ src/main-internal.aot.ts
@@ -1,7 +1,7 @@
 import { platformBrowser } from '@angular/platform-browser';
 import { AppModuleNgFactory } from './ngfactory/app/app.module.ngfactory';
 
-import { SkyAppBootstrapper } from '../runtime/bootstrapper';
+import { SkyAppBootstrapper } from '@blackbaud/skyux-builder/runtime/bootstrapper';
 
 SkyAppBootstrapper.processBootstrapConfig().then(() => {
   platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
