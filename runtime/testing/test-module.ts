7cd9e2c80db443317e8ebbad75afcbbe1b946029
Paul Crowder <paul.crowder@blackbaud.com>
2017-02-23 15:45:30 -0500

    Added test module that can be imported into TestBed

---
+++ runtime/testing/test-module.ts
@@ -0,0 +1,10 @@
+import {
+  NgModule
+} from '@angular/core';
+
+import { SkyPagesModule } from '../../src/app/sky-pages.module';
+
+@NgModule({
+  imports: [SkyPagesModule]
+})
+export class SkyAppTestModule { }
