cdc798979ddfdae39c8ab42d3dae01dbcffa605d
Paul Crowder <paul.crowder@blackbaud.com>
2017-02-28 11:24:21 -0500

    Spacing fixes

--- runtime/testing/test-module.ts
+++ runtime/testing/test-module.ts
@@ -1,7 +1,7 @@
-import {
-  NgModule
-} from '@angular/core';
-import {APP_BASE_HREF} from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { APP_BASE_HREF } from '@angular/common';
+
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { SkyPagesModule } from '../../src/app/sky-pages.module';
