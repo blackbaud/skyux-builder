a2a467a492e002d011f62aed5b4487679475a4b3
Bobby Earl <bobby.earl@blackbaud.com>
2017-03-14 12:14:50 -0400

    Removed config flag.  Using command context instead of environments.  Auto extending configs

--- config/sky-pages/sky-pages.config.js
+++ config/sky-pages/sky-pages.config.js
@@ -19,11 +19,6 @@ function resolve(root, args) {
 }
 
 function readConfig(file) {
-  if (!fs.existsSync(file)) {
-    logger.error(`Unable to locate config file ${file}`);
-    return {};
-  }
-
   return JSON.parse(fs.readFileSync(file, 'utf8'));
 }
 
@@ -37,51 +32,23 @@ module.exports = {
    * @param {argv} Optional arguments from command line
    * @returns [SkyPagesConfig] skyPagesConfig
    */
-  getSkyPagesConfig: function (argv) {
+  getSkyPagesConfig: function (command) {
 
-    const localEnvConfig = this.spaPath(`skyuxconfig.${process.env.SKYUX_ENV}.json`);
-    const localBaseConfig = this.spaPath(`skyuxconfig.json`);
-    let configs = [];
+    const base = readConfig(this.outPath(`skyuxconfig.json`));
+    const local = this.spaPath(`skyuxconfig.json`);
+    const cmd = this.spaPath(`skyuxconfig.${command}.json`);
 
-    // Builder's base config always is the baseline
-    let config = readConfig(this.outPath(`skyuxconfig.json`));
-
-    // Allow for shorthand -c
-    if (argv && argv.c) {
-      argv.config = argv.c;
+    if (fs.existsSync(local)) {
+      logger.info(`Merging local config skyuxconfig.json`);
+      merge.recursive(base, readConfig(local));
     }
 
-    // Order of precedence...
-    // 1. Config flag passed in.
-    // 2. SKYUX_ENV set and matching config file found
-    // 3. Local skyuxconfig.json file found
-    if (argv && argv.config) {
-      logger.info(`Processing config file ${argv.config}`);
-      configs.push(readConfig(argv.config));
-    } else if (process.env.SKYUX_ENV && fs.existsSync(localEnvConfig)) {
-      logger.info(`Processing config file ${localEnvConfig}`);
-      configs.push(readConfig(localEnvConfig));
-    } else if (fs.existsSync(localBaseConfig)) {
-      logger.info(`Processing config file ${localBaseConfig}`);
-      configs.push(readConfig(localBaseConfig));
-    } else {
-      logger.info('Using default skyuxconfig.json configuration.');
+    if (fs.existsSync(cmd)) {
+      logger.info(`Merging command config skyuxconfig.${command}.json`);
+      merge.recursive(base, readConfig(cmd));
     }
 
-    if (configs.length) {
-
-      // Recursively process any "extends", reading from last config
-      while (configs[configs.length - 1].extends) {
-        const extendsFrom = path.resolve(configs[configs.length - 1].extends);
-        logger.info(`Processing config file ${extendsFrom}`);
-        configs.push(readConfig(extendsFrom));
-      }
-
-      // Finally, merge our configs in, in reverse
-      configs.reverse().forEach(c => merge.recursive(config, c));
-    }
-
-    return config;
+    return base;
   },
 
   /**
--- test/config-sky-pages.spec.js
+++ test/config-sky-pages.spec.js
@@ -21,167 +21,43 @@ describe('config sky-pages', () => {
     expect(appBase).toEqual('/' + name + '/');
   });
 
-  it('should handle no local configuration files', () => {
-    let hasBaseline = false;
+  it('should load the local skyuxconfig.json file if it exists', () => {
     spyOn(logger, 'info');
-    spyOn(fs, 'existsSync').and.callFake(file => {
-      if (!hasBaseline) {
-        hasBaseline = true;
-        return true;
-      }
-
-      return false;
-    });
+    spyOn(fs, 'existsSync').and.callFake(f => f.indexOf('skyuxconfig.json') > -1);
     spyOn(fs, 'readFileSync').and.returnValue(JSON.stringify({
       baseline: true
     }));
 
     const lib = require('../config/sky-pages/sky-pages.config');
-    const config = lib.getSkyPagesConfig();
-
-    console.log(config);
-
+    const config = lib.getSkyPagesConfig('build');
     expect(config.baseline).toEqual(true);
     expect(logger.info).toHaveBeenCalledWith(
-      `Using default skyuxconfig.json configuration.`
+      `Merging local config skyuxconfig.json`
     );
   });
 
-  it('should use the local skyuxconfig.json as the default, if it exists', () => {
+  it('should local the matching skyuxconfig.command.json file if it exists', () => {
+    const cmd = 'asdf';
+
     spyOn(logger, 'info');
-    spyOn(fs, 'existsSync').and.callFake(f => f.indexOf('skyuxconfig.json') > -1);
+    spyOn(fs, 'existsSync').and.callFake(f => f.indexOf(`skyuxconfig.${cmd}.json`) > -1);
     spyOn(fs, 'readFileSync').and.returnValue(JSON.stringify({
-      custom: true
+      baseline: true
     }));
 
     const lib = require('../config/sky-pages/sky-pages.config');
-    const config = lib.getSkyPagesConfig();
-    expect(config.custom).toEqual(true);
+    const config = lib.getSkyPagesConfig(cmd);
+    expect(config.baseline).toEqual(true);
     expect(logger.info).toHaveBeenCalledWith(
-      `Processing config file ${lib.spaPath('skyuxconfig.json')}`
+      `Merging command config skyuxconfig.${cmd}.json`
     );
   });
 
-  it('should read the shorthand -c flag', () => {
-    spyOn(logger, 'info');
-    spyOn(fs, 'existsSync').and.callFake(f => f.indexOf('test.json') > -1);
-    spyOn(fs, 'readFileSync').and.returnValue(JSON.stringify({
-      custom: true
-    }));
-
-    const lib = require('../config/sky-pages/sky-pages.config');
-    const config = lib.getSkyPagesConfig({
-      c: 'test.json'
-    });
-    expect(config.custom).toEqual(true);
-    expect(logger.info).toHaveBeenCalledWith(
-      `Processing config file test.json`
-    );
-  });
-
-  it('should read the --config flag', () => {
-    spyOn(logger, 'info');
-    spyOn(fs, 'existsSync').and.callFake(f => f.indexOf('test.json') > -1);
-    spyOn(fs, 'readFileSync').and.returnValue(JSON.stringify({
-      custom: true
-    }));
-
-    const lib = require('../config/sky-pages/sky-pages.config');
-    const config = lib.getSkyPagesConfig({
-      config: 'test.json'
-    });
-    expect(config.custom).toEqual(true);
-    expect(logger.info).toHaveBeenCalledWith(
-      `Processing config file test.json`
-    );
-  });
-
-  it('should handle the SKYUX_ENV environment variable and a matching file', () => {
-    spyOn(logger, 'info');
-    spyOn(fs, 'existsSync').and.callFake(f => f.indexOf('skyuxconfig.env.json') > -1);
-    spyOn(fs, 'readFileSync').and.returnValue(JSON.stringify({
-      custom: true
-    }));
-
-    process.env.SKYUX_ENV = 'env';
-    const lib = require('../config/sky-pages/sky-pages.config');
-    const config = lib.getSkyPagesConfig();
-    expect(config.custom).toEqual(true);
-    expect(logger.info).toHaveBeenCalledWith(
-      `Processing config file ${lib.spaPath('skyuxconfig.env.json')}`
-    );
-  });
-
-  it('should handle the SKYUX_ENV environment variable without a matching file', () => {
+  it('should work if no config files exist', () => {
     spyOn(fs, 'existsSync').and.returnValue(false);
-    process.env.SKYUX_ENV = 'env';
     const lib = require('../config/sky-pages/sky-pages.config');
-    const config = lib.getSkyPagesConfig();
-    expect(config.custom).not.toEqual(true);
-    delete process.env.SKYUX_ENV;
-  });
-
-  it('should recursively follow the extends property of a config file', () => {
-    spyOn(logger, 'info');
-    spyOn(fs, 'existsSync').and.returnValue(true);
-    spyOn(fs, 'readFileSync').and.callFake(file => {
-      let config = '';
-      if (file.indexOf('skyuxconfig.json') > -1) {
-        config = {
-          extends: 'A.json',
-          test: 'asdf',
-          test4: 'jkl',
-          test5: '1234',
-          test1: true
-        };
-      } else if (file.indexOf('A.json') > -1) {
-        config = {
-          extends: 'B.json',
-          test: '1234',
-          test4: 'asdf',
-          test2: false
-        };
-      } else if (file.indexOf('B.json') > -1) {
-        config = {
-          test: 'jkl',
-          test3: true
-        };
-      }
-
-      return JSON.stringify(config);
-    });
-
-    const lib = require('../config/sky-pages/sky-pages.config');
-    const config = lib.getSkyPagesConfig();
-    expect(config.test).toEqual('asdf');
-    expect(config.test1).toEqual(true);
-    expect(config.test2).toEqual(false);
-    expect(config.test3).toEqual(true);
-    expect(config.test4).toEqual('jkl');
-    expect(config.test5).toEqual('1234');
-    expect(logger.info).toHaveBeenCalledWith(
-      `Processing config file ${lib.spaPath('skyuxconfig.json')}`
-    );
-    expect(logger.info).toHaveBeenCalledWith(
-      `Processing config file ${lib.spaPath('A.json')}`
-    );
-    expect(logger.info).toHaveBeenCalledWith(
-      `Processing config file ${lib.spaPath('B.json')}`
-    );
-  });
-
-  it('should handle files that do not exist', () => {
-    spyOn(logger, 'error');
-    spyOn(fs, 'existsSync').and.callFake(f => f.indexOf('test.json') === -1);
-
-    const lib = require('../config/sky-pages/sky-pages.config');
-    const config = lib.getSkyPagesConfig({
-      config: 'test.json'
-    });
-    expect(config.custom).not.toEqual(true);
-    expect(logger.error).toHaveBeenCalledWith(
-      `Unable to locate config file test.json`
-    );
+    const config = lib.getSkyPagesConfig('build');
+    expect(config.baseline).not.toEqual(true);
   });
 
 });
