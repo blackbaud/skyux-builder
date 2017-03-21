19943499ea387f11965c6ccb9c4d35d6cc973f22
Paul Crowder <paul.crowder@blackbaud.com>
2017-03-21 16:32:28 -0400

    Load SKY UX styles before running unit tests (#95)

---
+++ utils/spec-styles.js
@@ -0,0 +1,14 @@
+/*jshint node: true, jasmine: true */
+
+'use strict';
+
+var styleLoader = require('./sky-style-loader');
+
+// A race condition exists in Firefox where tests can begin before styles are loaded.
+// This will ensure that styles are loaded before tests run by ensuring the style rule
+// for the HTML hidden property defined in sky.scss has been applied.
+(function () {
+  beforeAll(function (done) {
+    styleLoader.loadStyles().then(done);
+  });
+}());
