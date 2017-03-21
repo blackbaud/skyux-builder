86bf691b4338f94fef324bc3eab0f91771dcf4a0
Paul Crowder <paul.crowder@blackbaud.com>
2017-03-21 13:13:40 -0400

    Load SKY UX styles before running unit tests

--- config/karma/shared.karma.conf.js
+++ config/karma/shared.karma.conf.js
@@ -24,9 +24,14 @@ function getConfig(config) {
       {
         pattern: '../../utils/spec-bundle.js',
         watched: false
+      },
+      {
+        pattern: '../../utils/spec-styles.js',
+        watched: false
       }
     ],
     preprocessors: {
+      '../../utils/spec-styles.js': ['webpack'],
       '../../utils/spec-bundle.js': ['coverage', 'webpack', 'sourcemap']
     },
     webpack: testWebpackConfig.getWebpackConfig(skyPagesConfig),
