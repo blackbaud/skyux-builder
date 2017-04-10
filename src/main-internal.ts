34e76854f41e967eb3313222ae472d7b150b570c
Paul Crowder <paul.crowder@blackbaud.com>
2017-04-10 17:36:27 -0400

    Removed log statements

--- src/main-internal.ts
+++ src/main-internal.ts
@@ -3,9 +3,6 @@ import { AppModule } from './app/app.module';
 
 import { SkyAppBootstrapper } from '../runtime/bootstrapper';
 
-console.time('processing bootstrap config');
 SkyAppBootstrapper.processBootstrapConfig().then(() => {
   platformBrowserDynamic().bootstrapModule(AppModule);
-  console.timeEnd('processing bootstrap config');
-  console.log('processing bootstrap config done ' + new Date().getTime());
 });
