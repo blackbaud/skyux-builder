5cc5662b3bf054ec7b3c2ac279faaf4b5c6b2b1a
Blackbaud-PatrickOFriel <patrick.ofriel@blackbaud.com>
2017-04-17 16:08:53 -0400

    add test logging

--- utils/spec-styles.js
+++ utils/spec-styles.js
@@ -9,6 +9,10 @@ var styleLoader = require('./sky-style-loader');
 // for the HTML hidden property defined in sky.scss has been applied.
 (function () {
   beforeAll(function (done) {
-    styleLoader.loadStyles().then(done, done);
+    styleLoader.loadStyles().then(function () {
+      console.log('before done');
+      done();
+      console.log('after done');
+    });
   });
 }());
