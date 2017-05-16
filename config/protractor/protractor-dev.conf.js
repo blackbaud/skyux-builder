b319147cb17d44613708d9b407640e3bd5df0ae4
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-05-16 14:28:25 -0400

    Using template branch for testing

--- config/protractor/protractor-dev.conf.js
+++ config/protractor/protractor-dev.conf.js
@@ -16,12 +16,13 @@ let config = {
   },
   onPrepare: () => {
 
-    const url = 'https://github.com/blackbaud/skyux-template.git#update-package-dependencies';
+    const url = 'https://github.com/blackbaud/skyux-template';
     jasmine.getEnv().addReporter(new SpecReporter());
 
     return new Promise((resolve, reject) => {
       common.exec(`rm`, [`-rf`, `${common.tmp}`])
         .then(() => common.exec(`git`, [`clone`, `${url}`, `${common.tmp}`]))
+        .then(() => common.exec(`git`, [`checkout`, `update-package-dependencies`]))
         .then(() => common.exec(`npm`, [`i`, '--only=prod'], common.cwdOpts))
         .then(() => common.exec(`npm`, [`i`, `../`], common.cwdOpts))
         .then(resolve)
