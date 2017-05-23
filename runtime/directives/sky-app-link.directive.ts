97c0776e326b9d202e91703b501701ae4bd7c7b7
Bobby Earl <bobby.earl@blackbaud.com>
2017-05-23 10:11:08 -0400

    Updated directive tests

--- runtime/directives/sky-app-link.directive.ts
+++ runtime/directives/sky-app-link.directive.ts
@@ -1,33 +1,28 @@
-import { Directive, Input, OnInit } from '@angular/core';
+import { Directive, Input } from '@angular/core';
 import { LocationStrategy } from '@angular/common';
 import { ActivatedRoute, Router, RouterLinkWithHref } from '@angular/router';
 import { SkyAppConfig } from '../config';
+import { RuntimeConfigParams } from '../params';
 
 @Directive({
   selector: '[skyAppLink]'
 })
-export class SkyAppLinkDirective extends RouterLinkWithHref implements OnInit {
-
-  private activatedRoute: ActivatedRoute;
-  private skyAppConfig: SkyAppConfig;
+export class SkyAppLinkDirective extends RouterLinkWithHref {
 
   @Input()
   set skyAppLink(commands: any[]|string) {
     this.routerLink = commands;
   }
 
-  public ngOnInit() {
-    console.log(this.skyAppConfig.runtime.params);
-    this.queryParams = this.skyAppConfig.runtime.params.getAll();
-  }
-
   constructor(
     router: Router,
     route: ActivatedRoute,
     locationStrategy: LocationStrategy,
-    skyAppConfig: SkyAppConfig
+    private skyAppConfig: SkyAppConfig
   ) {
     super(router, route, locationStrategy);
-    this.activatedRoute = route;
+
+    const runtimeConfigParams: RuntimeConfigParams = skyAppConfig.runtime.params;
+    this.queryParams = runtimeConfigParams.getAll();
   }
 }
