d0730718cb45d4567add682724b81e76d79de3e4
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-05-25 12:06:29 -0400

    Added style loader to runtime

---
+++ runtime/sky-style-loader.js
@@ -0,0 +1,41 @@
+/*jshint node: true*/
+
+'use strict';
+
+require('style-loader!@blackbaud/skyux/dist/css/sky.css');
+
+var FontFaceObserver = require('fontfaceobserver');
+
+var stylesAreLoaded = false;
+
+var LOAD_TIMEOUT = 30000;
+
+module.exports = {
+  loadStyles: function () {
+    var fontAwesome = new FontFaceObserver('FontAwesome');
+    var openSans = new FontFaceObserver('Open Sans');
+    var oswald = new FontFaceObserver('Oswald');
+    var promise;
+
+    promise = Promise.all(
+      [
+        // Specify a character for FontAwesome since some browsers will fail to detect
+        // when the font is loaded unless a known character with a different width
+        // than the default is not specified.
+        fontAwesome.load('\uf0fc', LOAD_TIMEOUT),
+        openSans.load(null, LOAD_TIMEOUT),
+        oswald.load(null, LOAD_TIMEOUT)
+      ]
+    );
+
+    promise.then(function () {
+      stylesAreLoaded = true;
+    });
+
+    return promise;
+  },
+
+  stylesAreLoaded: function () {
+    return stylesAreLoaded;
+  }
+};
