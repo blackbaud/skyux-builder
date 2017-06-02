6bac4132df4c4e3e6b18a16012a0e6aa852d75b9
Blackbaud-MattGregg <matt.gregg@blackbaud.com>
2017-06-01 21:51:15 -0400

    Add better output with help, DOM targets and HTML

--- runtime/testing/e2e/a11y.ts
+++ runtime/testing/e2e/a11y.ts
@@ -1,21 +1,47 @@
 /*jslint node: true */
 'use strict';
 
-var AxeBuilder = require('axe-webdriverjs');
+const AxeBuilder = require('axe-webdriverjs');
 import { browser } from 'protractor';
 
-export class A11yTester {
+const red = "\x1b[31m";
+
+export class A11yTest {
 
   //public static run(testName, driver): any {
   public static run(): any {
     const config = JSON.parse(browser.params.skyPagesConfig);
     const options = config.skyux.appSettings.a11y;
 
+    console.log(options.runOnly.values.length);
     // const skyuxConfig = this.config.skyux;
     AxeBuilder(browser.driver)
       .options(options)
       .analyze(function (results) {
-        console.log(results);
+        //console.log(results);
+        var numResults = results.violations.length;
+
+        if (numResults === 0) {
+          return numResults;
+        }
+        else if (numResults > 0) {
+          console.log(red, "Accessibility failure(s) found for: " + results.url + "\n");
+
+          results.violations.forEach(result => {
+            var label = result.nodes.length === 1 ? ' element ' : ' elements ';
+            //const issues = result.nodes.reduce(function(msg, node) {
+//            });
+            var msg = result.nodes.reduce(function(msg, node) {
+                  return msg + "   Location: " + node.target[0] +  "\n   " + node.html + "\n";
+                }, "\n");
+            msg = result.nodes.length + label + "failed '" + result.id + "' rule: " + result.help + msg;
+            console.log(red, msg + "   - Get help at: " + result.helpUrl + "\n");
+
+          });
+          return numResults;
+        }
+
     });
   }
+
 }
