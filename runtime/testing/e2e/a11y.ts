4cd379c56f3cd64614e14274f4763b0b3b7741e8
Blackbaud-MattGregg <matt.gregg@blackbaud.com>
2017-06-05 23:40:14 -0400

    Asynchronous callback, additional log function cleanup, all rules included by default - config as rules excluded

--- runtime/testing/e2e/a11y.ts
+++ runtime/testing/e2e/a11y.ts
@@ -4,43 +4,42 @@
 const AxeBuilder = require('axe-webdriverjs');
 import { browser } from 'protractor';
 
-const red = "\x1b[31m";
-
 export class A11yTest {
 
-  //public static run(testName, driver): any {
-  public static run(): any {
+  public static log(results): any {
+      //console.error(`${violations}`);
+      var numResults = results.violations.length;
+
+      if (numResults > 0) {
+        console.error("Accessibility failure(s) found for: " + results.url + "\n");
+        results.violations.forEach(result => {
+          var label = result.nodes.length === 1 ? 'element' : 'elements';
+          var msg = result.nodes.reduce(function(msg, node) {
+              return `${msg}  Location: ${node.target[0]}
+  ${node.html}`;
+          }, "\n");
+          console.error(`${result.nodes.length} ${label} failed '${result.id}' rule: ${result.help} ${msg}
+  Get help at: ${result.helpUrl}.
+`)
+        });
+      }
+  }
+
+  public static run(): Promise<Number> {
     const config = JSON.parse(browser.params.skyPagesConfig);
     const options = config.skyux.appSettings.a11y;
 
-    console.log(options.runOnly.values.length);
-    // const skyuxConfig = this.config.skyux;
-    AxeBuilder(browser.driver)
-      .options(options)
-      .analyze(function (results) {
-        //console.log(results);
-        var numResults = results.violations.length;
-
-        if (numResults === 0) {
-          return numResults;
-        }
-        else if (numResults > 0) {
-          console.log(red, "Accessibility failure(s) found for: " + results.url + "\n");
-
-          results.violations.forEach(result => {
-            var label = result.nodes.length === 1 ? ' element ' : ' elements ';
-            //const issues = result.nodes.reduce(function(msg, node) {
-//            });
-            var msg = result.nodes.reduce(function(msg, node) {
-                  return msg + "   Location: " + node.target[0] +  "\n   " + node.html + "\n";
-                }, "\n");
-            msg = result.nodes.length + label + "failed '" + result.id + "' rule: " + result.help + msg;
-            console.log(red, msg + "   - Get help at: " + result.helpUrl + "\n");
-
-          });
-          return numResults;
-        }
-
+    return new Promise(resolve => {
+      AxeBuilder(browser.driver)
+        .options(options)
+        .analyze(results => {
+          const violations = results.violations.length;
+          if (violations) {
+            A11yTest.log(results);
+          }
+          var fail = results.violations.length === 1 ? ' failure' : ' failures';
+          resolve(violations + fail);
+        });
     });
   }
 
