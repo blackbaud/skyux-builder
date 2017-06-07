808c60d26edadcb0bc03df20ab92d13e3fa091ce
Brandon Stirnaman <brandon.stirnaman@blackbaud.com>
2017-06-07 11:41:41 -0400

    Add e2e test for guard

--- e2e/skyux-build-jit.e2e-spec.js
+++ e2e/skyux-build-jit.e2e-spec.js
@@ -3,28 +3,55 @@
 const common = require('./shared/common');
 const tests = require('./shared/tests');
 
-describe('skyux build jit', () => {
+function prepareBuild() {
+  const opts = { mode: 'easy', name: 'dist', compileMode: 'jit' };
+  return common.prepareBuild(opts)
+    .catch(err => console.error);
+}
 
-  beforeAll((done) => {
-    const opts = { mode: 'easy', name: 'dist', compileMode: 'jit' };
-    common.prepareBuild(opts)
-      .then(done)
-      .catch(err => {
-        console.log(err);
-        done();
-      });
+describe('skyux build jit', () => {
+  describe('w/base template', () => {
+    beforeAll((done) => prepareBuild().then(done));
+
+    it('should have exitCode 0', tests.verifyExitCode);
+
+    it('should generate expected static files', tests.verifyFiles);
+
+    it('should render the home components', tests.renderHomeComponent);
+
+    it('should render shared nav component', tests.renderSharedNavComponent);
+
+    it('should follow routerLink and render about component', tests.followRouterLinkRenderAbout);
+
+    afterAll(common.afterAll);
   });
 
-  afterAll(common.afterAll);
+  describe('w/guard', () => {
+    beforeAll((done) => {
+      const guard = `
+import { Injectable } from '@angular/core';
 
-  it('should have exitCode 0', tests.verifyExitCode);
+@Injectable()
+export class AboutGuard {
+  canActivate(next: any, state: any) {
+    return false;
+  }
+}
+`;
 
-  it('should generate expected static files', tests.verifyFiles);
+      common.writeAppFile('about/index.guard.ts', guard)
+        .then(() => prepareBuild())
+        .then(done)
+        .catch(console.error);
+    });
 
-  it('should render the home components', tests.renderHomeComponent);
+    it('should not follow routerLink when guard returns false', tests.respectGuardCanActivate);
 
-  it('should render shared nav component', tests.renderSharedNavComponent);
-
-  it('should follow routerLink and render about component', tests.followRouterLinkRenderAbout);
-
+    afterAll((done) => {
+      common.removeAppFile('about/index.guard.ts')
+        .then(() => common.afterAll())
+        .then(done)
+        .catch(console.error);
+    });
+  });
 });
