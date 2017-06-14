76eeb64307cf1763deb96daa252aa82b55a74aef
Bobby Earl <bobby.earl@blackbaud.com>
2017-06-14 18:11:51 -0400

    Small performance refactor

--- runtime/params.ts
+++ runtime/params.ts
@@ -1,5 +1,19 @@
 import { URLSearchParams } from '@angular/http';
 
+/**
+ * Given a "url" (could be just querystring, or fully qualified),
+ * Returns the extracted URLSearchParams.
+ * @param {string} url
+ * @return {URLSearchParams} urlSearchParams
+ */
+function getUrlSearchParams(url: string): URLSearchParams {
+  if (url.indexOf('?') > -1) {
+    url = url.split('?')[1];
+  }
+
+  return new URLSearchParams(url);
+}
+
 export class SkyAppRuntimeConfigParams {
 
   private params: {[key: string]: string} = {};
@@ -57,7 +71,7 @@ export class SkyAppRuntimeConfigParams {
    * @returns {string} url
    */
   public getUrl(url: string): string {
-    const urlSearchParams: URLSearchParams = this.getUrlSearchParams(url);
+    const urlSearchParams: URLSearchParams = getUrlSearchParams(url);
     const delimiter = url.indexOf('?') === -1 ? '?' : '&';
     let joined: string[] = [];
 
@@ -68,20 +82,6 @@ export class SkyAppRuntimeConfigParams {
     });
 
     return joined.length === 0 ? url : `${url}${delimiter}${joined.join('&')}`;
-  }
-
-  /**
-   * Given a "url" (could be just querystring, or fully qualified),
-   * Returns the extracted URLSearchParams.
-   * @param {string} url
-   * @return {URLSearchParams} urlSearchParams
-   */
-  private getUrlSearchParams(url: string): URLSearchParams {
-    if (url.indexOf('?') > -1) {
-      url = url.split('?')[1];
-    }
-
-    return new URLSearchParams(url);
   }
 
 }
