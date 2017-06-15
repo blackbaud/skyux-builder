93d7aaad7b45027e2b42c75da3d9f7e53d189fb1
Blackbaud-MattGregg <matt.gregg@blackbaud.com>
2017-06-15 14:40:38 -0400

    Removed redundant if statement from log method

--- runtime/testing/e2e/a11y.ts
+++ runtime/testing/e2e/a11y.ts
@@ -7,20 +7,17 @@ import { browser } from 'protractor';
 export class SkyA11y {
 
   public static log(results): any {
-      const numResults = results.violations.length;
-      if (numResults > 0) {
-        console.error("Accessibility failure(s) found for: " + results.url + "\n");
-        results.violations.forEach(result => {
-          var label = result.nodes.length === 1 ? 'element' : 'elements';
-          var msg = result.nodes.reduce(function(msg, node) {
-              return `${msg}  Location: ${node.target[0]}
+      console.error("Accessibility failure(s) found for: " + results.url + "\n");
+      results.violations.forEach(result => {
+        var label = result.nodes.length === 1 ? 'element' : 'elements';
+        var msg = result.nodes.reduce(function(msg, node) {
+            return `${msg}  Location: ${node.target[0]}
   ${node.html}`;
-          }, "\n");
-          console.error(`${result.nodes.length} ${label} failed '${result.id}' rule: ${result.help} ${msg}
+        }, "\n");
+        console.error(`${result.nodes.length} ${label} failed '${result.id}' rule: ${result.help} ${msg}
   Get help at: ${result.helpUrl}
 `)
-        });
-      }
+      });
   }
 
   public static run(): Promise<Number> {
