fb7a713f08994b8462d1f22537bde9f7944c67c6
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-06-16 10:39:14 -0400

    Added ignore pattern to component generator

--- config/sky-pages/sky-pages.config.js
+++ config/sky-pages/sky-pages.config.js
@@ -71,6 +71,7 @@ module.exports = {
         },
         command: command,
         componentsPattern: '**/*.component.ts',
+        componentsIgnorePattern: './public/**/*.component.ts',
         includeRouteModule: true,
         routesPattern: '**/index.html',
         runtimeAlias: 'sky-pages-internal/runtime',
