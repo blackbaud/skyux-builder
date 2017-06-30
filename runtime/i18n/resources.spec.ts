6f96723d852b29046f1402101288ea4feae56ac4
BLACKBAUD\manoj.kurapati <manoj.kurapati@blackbaud.com>
2017-06-30 09:42:22 -0400

    Wrote Unit Tests

--- runtime/i18n/resources.spec.ts
+++ runtime/i18n/resources.spec.ts
@@ -1,32 +1,19 @@
 import { SkyAppResourcesPipe } from './resources';
-import { * } from './fixtures';
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-describe('Error Component', () => {
-  // let fixture: ComponentFixture<SkyAppResourcesPipe>;
-  // let cmp: SkyAppResourcesPipe;
-  // let de:  DebugElement;
-  // let el: HTMLElement;
+describe('SkyAppResourcesPipe', () => {
+
   let pipe: SkyAppResourcesPipe;
-
   beforeEach(() => {
-    // TestBed.configureTestingModule({
-    //   imports: [],
-    //   declarations: [
-    //     ESkyAppResourcesPipe
-    //   ]
-    // });
-    pipe = new SkyAppResourcesPipe();
-    pipe.resources = require('json-loader!./fixtures/resources_en_US.mock.json');
+    pipe = new SkyAppResourcesPipe('.\\/fixtures\\/resources_en_US.json');
   });
 
-  fit('should ensure that a defined hello_world identifier returns Hello World', () => {
-    let str: string = pipe.transform('hello_world')
+  it('should ensure that a defined hello_world identifier returns Hello World', () => {
+    let str: string = pipe.transform('hello_world');
     expect(str).toBe('Hello World');
   });
 
-  fit('should ensure that an undefined goodbye_world identifier returns goodbye_world', () => {
-    let str: string = pipe.transform('goodbye_world')
+  it('should ensure that an undefined goodbye_world identifier returns goodbye_world', () => {
+    let str: string = pipe.transform('goodbye_world');
     expect(str).toBe('goodbye_world');
   });
 });
--- runtime/i18n/resources.ts
+++ runtime/i18n/resources.ts
@@ -4,11 +4,20 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'skyAppResources'
 })
 export class SkyAppResourcesPipe implements PipeTransform {
-  public resources: any = require('json-loader!sky-pages-spa/src/assets/locales/resources_en_US.json');
+  public resources: any;
+
+  constructor(resource?: string) {
+    if (resource.length > 0) {
+      this.resources = require('json-loader!' + resource);
+    } else {
+      this.resources = require
+      ('json-loader!sky-pages-spa/src/assets/locales/resources_en_US.json');
+    }
+  }
 
   public transform(val) {
     let stringObj: {_description: string, message: string} = this.resources[val];
-    if(stringObj){
+    if (stringObj) {
       return stringObj.message;
     } else {
       return val;
