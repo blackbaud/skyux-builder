1cbbf40799da1ada1088ba5b86401a142ab631d9
Steve Brush <steve.brush@blackbaud.com>
2017-07-26 12:11:46 -0400

    Added `skyux lint` command (#205)
    * Update angular (#189)
    
    * Updated dependencies
    
    * Updated skyux2 version
    
    * Updated dependencies
    
    * Using template branch for testing
    
    * Reverted protractor config
    
    * Pointing to dev branch for local testing
    
    * Updated package json
    
    * Using explicit types
    
    * Updated version of skyux
    
    * Updated dependencies
    
    * Fixed linting errors
    
    * Added fontfaceobserver to types
    
    * Fixed tslint errors
    
    * Fixed tslint errors
    
    * Ensure that the files are not directories
    
    * Update tests for file processor
    
    * Update plugin-file-processor.js
    
    * Update plugin-file-processor.spec.js
    
    * Updated version of skyux
    
    * Updated config
    
    * Added specific version of firefox
    
    * Updated tsconfig
    
    * Updating travis version
    
    * Updated version of firefox
    
    * Updated directconnect
    
    * Updated protractor
    
    * Added firfox launcher
    
    * Removed directConnect
    
    * Reverting back to directConnect
    
    * Removed firefox
    
    * Re-added firefox
    
    * Accepting insecure certs
    
    * Enabled directconnect
    
    * use chrome with chromOptions in protractor-dev.conf.js
    
    * add sources to travis.yml
    
    * add dist: trusty
    
    * Removed tsconfig paths
    
    * Update typescript-loader
    
    * Simpler method for avoiding directories
    
    * As ts-node option and fix e2e test
    
    * updates for rc.0 release (#190)
    
    * Omnibar config (#193)
    
    * Using omnibarConfigMap for envid/svcid
    
    * Fixing implicit any
    
    * Fixed codelyzer path (#192)
    
    * Fixed codelyzer path
    
    * Added node_modules to excludes
    
    * Readded node_modules
    
    * Ignore public directory when generating components (#187)
    
    * Added ignore pattern to component generator
    
    * Component generator should ignore public folder
    
    * Updated ignore pattern
    
    * Plugin File Processor should not check directories (#186)
    
    * Do not check directories
    
    * Added nodir option
    
    * Updated CHANGELOG.md and package.json for 1.0.0-rc.1 (#194)
    
    * Updated template branch, bug fix for component pattern (#195)
    
    * Updated branch to master
    
    * Fixed bug with ignore components pattern
    
    * Updated unit tests
    
    * Release 1.0.0 rc.2 (#196)
    
    * Update CHANGELOG.md
    
    * Update package.json
    
    * Remove extra s. (#197)
    
    * Fixed type error, updated SKY UX (#199)
    
    * Fixed type error
    
    * Updated skyux
    
    * Updated skyux
    
    * Release 1.0.0 rc.3 (#200)
    
    * Fixed type error
    
    * Updated skyux
    
    * Updated skyux
    
    * Update package.json
    
    * Update CHANGELOG.md
    
    * Param functionality (#201)
    
    * Fixed typo in OmnibarConfigMap.  Allowing querystring params to be case insensitive
    
    * Test cleanup
    
    * Removed unnecessary mapping functionality
    
    * Added lint command
    
    * Fixed `skyux test/watch` performance (#202)
    
    * Removed tslint-loader
    * Created custom sky-tslint loader
    
    * Release 1.0.0-rc.4 (#207)
    
    * Updated changelog, version
    
    * Update CHANGELOG.md
    
    * Update CHANGELOG.md
    
    * Added to skyux builder (#204)
    
    * Updated package dependencies (#208)
    
    * Updated package dependencies
    
    * Update skyux version
    
    * use appropriate template branch
    
    * Add hash routing option for easy mode. (#206)
    
    * Updates rc5 (#209)
    
    * Updates for rc.5
    
    * Capitalize Angular. (#211)
    
    * Wrote failing test.  Then made config property public for template (#212)
    
    * Added skyux lint files
    
    * Update CHANGELOG.md
    
    * Updated unit tests
    
    * Updated unit tests
    
    * Fixed e2e test
    
    * Fixed e2e tests
    
    * Update build.js
    
    * Added colors to logger
    
    * Updated linter result
    
    * Added error log after karma reporter
    
    * Update test.js
    
    * Added errors to linter result

--- src/app/sky-pages.module.ts
+++ src/app/sky-pages.module.ts
@@ -1,6 +1,6 @@
 // File dynamically generated via webpack.
 // Matching definition makes IDE's happy.
-export let SkyPagesModule: any;
+export class SkyPagesModule {}
 
 // The following comment is used to track timestamps in `skyux serve`
 // TS (1492006796510 END
