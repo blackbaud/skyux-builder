33888e4be30ddafd93ec99bdd80c42bd98ad4bf4
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-08-04 11:52:54 -0400

    Code cleanup

--- config/protractor/plugins/sky-accessibility.js
+++ config/protractor/plugins/sky-accessibility.js
@@ -3,42 +3,48 @@ const logger = require('../../../utils/logger');
 
 function onPageStable(browser) {
   const context = this;
-  return browser.getCurrentUrl().then((url) => {
-    logger.info(`Starting accessibility checks for ${url}...`);
-    return new Promise((resolve) => {
-      axeBuilder(browser.driver)
-        .options(context.config.axe)
-        .analyze((results) => {
-          const numViolations = results.violations.length;
-          const subject = (numViolations === 1) ? 'violation' : 'violations';
 
-          logger.info(`Accessibility checks finished with ${numViolations} ${subject}.\n`);
+  return browser
+    .getCurrentUrl()
+    .then((url) => {
 
-          if (numViolations > 0) {
-            logViolations(results);
-          }
+      logger.info(`Starting accessibility checks for ${url}...`);
 
-          resolve();
-        });
-    });
+      return new Promise((resolve) => {
+        axeBuilder(browser.driver)
+          .options(context.config.axe)
+          .analyze((results) => {
+            const numViolations = results.violations.length;
+            const subject = (numViolations === 1) ? 'violation' : 'violations';
+
+            logger.info(`Accessibility checks finished with ${numViolations} ${subject}.\n`);
+
+            if (numViolations > 0) {
+              logViolations(results);
+            }
+
+            resolve();
+          });
+      });
   });
 }
 
 function logViolations(results) {
   results.violations.forEach((violation) => {
     const label = violation.nodes.length === 1 ? 'element' : 'elements';
+
     const wcagTags = violation.tags.filter((tags) => {
       return tags.match(/wcag\d{3}|^best*/gi);
     }).join(', ');
 
-    const message = violation.nodes.reduce((accumulator, node) => {
+    const html = violation.nodes.reduce((accumulator, node) => {
       return `${accumulator}\n${node.html}\n`;
     }, '       Elements:\n');
 
     const error = [
       `aXe - [Rule: ${violation.id}] ${violation.help} - WCAG: ${wcagTags}`,
       `       Get help at: ${violation.helpUrl}\n`,
-      `${message}\n\n`
+      `${html}\n\n`
     ].join('\n');
 
     logger.error(error);
