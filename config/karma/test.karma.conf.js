77b81ffb84f856f9b5e73b6880700af694732de7
Bobby Earl <bobby.earl@blackbaud.com>
2017-08-28 17:06:35 -0400

    Added support for browserstack configuration in karma

--- config/karma/test.karma.conf.js
+++ config/karma/test.karma.conf.js
@@ -8,6 +8,11 @@
  */
 function getConfig(config) {
   require('./shared.karma.conf')(config);
+
+  // This file is spawned so we'll need to read the args again
+  const minimist = require('minimist');
+  const argv = minimist(process.argv.slice(2));
+
   let configuration = {
     browsers: [
       'Chrome'
@@ -24,7 +29,41 @@ function getConfig(config) {
     configuration.browsers = ['Chrome_travis_ci'];
   }
 
-  config.set(configuration);
+  if (argv.browserstack) {
+
+    if (argv.username && argv.accessKey) {
+
+      const customLaunchers = {
+        bs_windows_chrome_latest: {
+          base: 'BrowserStack',
+          browser: 'chrome',
+          os: 'Windows',
+          os_version: '10'
+        }
+      };
+
+      configuration = {
+        browsers: Object.keys(customLaunchers),
+        customLaunchers: customLaunchers,
+        browserDisconnectTimeout: 3e5,
+        browserDisconnectTolerance: 3,
+        browserNoActivityTimeout: 3e5,
+        captureTimeout: 3e5,
+        browserStack: {
+          username: argv.username,
+          accessKey: argv.accessKey,
+          port: 9876,
+          pollingTimeout: 10000
+        }
+      };
+
+      config.set(configuration);
+      console.log('Configuring Karma to use Browserstack.');
+
+    } else {
+      console.error('Browserstack requires username and accessKey.')
+    }
+  }
 }
 
 module.exports = getConfig;
