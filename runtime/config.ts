a8bbec307afb5e35aa579c138f86b74289320ed9
bdjones2410 <brandon.jones@blackbaud.com>
2017-09-08 10:36:21 -0400

    updated interface

--- runtime/config.ts
+++ runtime/config.ts
@@ -31,18 +31,17 @@ export interface HelpWidgetConfig {
   defaultHelpKey?: string;
   locale?: string;
   helpBaseUrl?: string;
-  getChatData?: any;
-  customLocales?: any[];
+  customLocales?: string[];
   headerColor?: string;
   headerTextColor?: string;
   trainingCentralUrl?: string;
   knowledgeBaseUrl?: string;
   caseCentralUrl?: string;
   helpCentralUrl?: string;
-  hideUndock?: any;
-  hideWidgetOnMobile?: any;
-  hideHelpChat?: any;
-  useFlareSearch?: any;
+  hideUndock?: boolean;
+  hideWidgetOnMobile?: boolean;
+  hideHelpChat?: boolean;
+  useFlareSearch?: boolean;
 }
 
 export interface SkyuxConfigA11y {
