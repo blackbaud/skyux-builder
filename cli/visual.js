3fc980c69024e611323b79ef415b6e17b3d8438b
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-09-27 14:28:00 -0400

    Fixed linting errors

--- cli/visual.js
+++ cli/visual.js
@@ -1,4 +1,8 @@
+/*jslint node: true */
+'use strict';
+
 const path = require('path');
+const e2e = require('./e2e');
 
 function runVisualTests(argv, skyPagesConfig, webpack) {
   argv.config = path.resolve(
