fd647bd8b9e78e8a08433da61df4330971200d93
Bobby Earl <bobby.earl@blackbaud.com>
2017-10-11 14:12:00 -0400

    Using outPath() instead of __dirname.  Defaulting config-resolver to karma

--- cli/utils/config-resolver.js
+++ cli/utils/config-resolver.js
@@ -5,26 +5,32 @@ const path = require('path');
 const glob = require('glob');
 const fs = require('fs-extra');
 
+const skyPagesConfigUtil = require('../../config/sky-pages/sky-pages.config');
+
 function getPath(command, platform, root, dir) {
   let filename;
   switch (command) {
-    case 'test':
-    case 'watch':
-      filename = `config/karma/${command}.karma.conf.js`;
-    break;
-
     case 'e2e':
     case 'visual':
       filename = `config/protractor/protractor.conf.js`;
     break;
+
+    // Defaulting to karma so dev-runtime and src-app can be passed in via our test suite.
+    default:
+      filename = `config/karma/${command}.karma.conf.js`;
+    break;
   }
 
-  return path.resolve(root, dir, platform, filename);
+  const resolved = path.resolve(root, dir, platform, filename);
+  console.log(resolved);
+  return resolved;
 }
 
 function resolve(command, argv) {
 
   const platform = argv.platform || '';
+
+  console.log('LOOKING FOR: ' + command);
 
   // Using glob so we can find skyux-builder-config regardless of npm install location
   const external = glob.sync(getPath(
@@ -37,7 +43,7 @@ function resolve(command, argv) {
   const internal = getPath(
     command,
     platform,
-    __dirname,
+    skyPagesConfigUtil.outPath(),
     ''
   );
 
