da26d7f3d2b3c5bd97410b405351cedbdf1e11f1
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2017-11-10 15:13:24 -0500

    Added comment to plugin

--- plugin/output-keep-alive/index.js
+++ plugin/output-keep-alive/index.js
@@ -1,6 +1,15 @@
 /*jslint node: true */
 'use strict';
 
+/**
+ * For longer builds, this plugin periodically prints to the
+ * console to reset any timeouts associated with watched output.
+ * More info:
+ * https://docs.travis-ci.com/user/common-build-problems/#Build-times-out-because-no-output-was-received
+ *
+ * @name OutputKeepAlivePlugin
+ * @param {any} options
+ */
 function OutputKeepAlivePlugin(options = {}) {
   const printDot = () => process.stdout.write('.');
 
