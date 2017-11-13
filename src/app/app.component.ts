b05348c0237630929c4a45d70b1d611c2f749264
Paul Crowder <paul.crowder@blackbaud.com>
2017-11-13 13:40:16 -0500

    Fixed a bug preventing navigation via omnibar due to loading outside Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -156,11 +156,18 @@ export class AppComponent implements OnInit {
     }
 
     nav.beforeNavCallback = (item: BBOmnibarNavigationItem) => {
+      console.log(baseUrl);
       const url = item.url.toLowerCase();
 
       if (url.indexOf(baseUrl) === 0) {
         const routePath = item.url.substring(baseUrl.length, url.length);
-        this.router.navigateByUrl(routePath);
+
+        // Since the omnibar is loaded outside Angular, navigating needs to be explicitly
+        // run inside the Angular zone in order for navigation to work properly.
+        this.zone.run(() => {
+          this.router.navigateByUrl(routePath);
+        });
+
         return false;
       }
 
