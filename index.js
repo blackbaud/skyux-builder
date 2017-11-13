a9f129d9637ed325484193fdfde49e6fa20c7bec
Paul Crowder <paul.crowder@blackbaud.com>
2017-11-13 11:59:22 -0500

    Added ability to generate a component from the CLI

--- index.js
+++ index.js
@@ -48,6 +48,9 @@ module.exports = {
       case 'version':
         require('./cli/version')();
         break;
+      case 'generate':
+        require('./cli/generate')(argv);
+        break;
       default:
         return false;
     }
