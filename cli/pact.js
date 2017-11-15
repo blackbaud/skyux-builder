0ef05559275c2ec8a516819c5642db25dda7fdaa
Josh Landi <Josh.Landi@blackbaud.me>
2017-11-15 15:59:06 -0500

    writing tests for pact command, made some changes based on my findings during testing.

--- cli/pact.js
+++ cli/pact.js
@@ -7,7 +7,9 @@
  */
 function pact(command, argv) {
   const async = require('async');
-  const Server = require('karma').Server;
+  const configResolver = require('./utils/config-resolver');
+  const karma = require('karma');
+  const Server = karma.Server;
   const portfinder = require('portfinder');
   const url = require('url');
   const logger = require('../utils/logger');
@@ -20,32 +22,6 @@ function pact(command, argv) {
 
   argv = argv || process.argv;
   argv.command = command;
-
-  let lintResult;
-
-  const onRunStart = () => {
-    lintResult = tsLinter.lintSync();
-  };
-
-  const onRunComplete = () => {
-    if (lintResult.exitCode > 0) {
-      // Pull the logger out of the execution stream to let it print
-      // after karma's coverage reporter.
-      setTimeout(() => {
-        logger.error('Process failed due to linting errors:');
-        lintResult.errors.forEach(error => logger.error(error));
-      }, 10);
-    }
-  };
-
-  const onExit = (exitCode) => {
-    if (exitCode === 0) {
-      exitCode = lintResult.exitCode;
-    }
-
-    logger.info(`Karma has exited with ${exitCode}.`);
-    process.exit(exitCode);
-  };
 
   let pactPortSeries = [];
   let consumedPorts = [];
@@ -66,78 +42,110 @@ function pact(command, argv) {
         .then((port) => {
           consumedPorts.push(port);
           callback(null, port);
-        });
+        })
+        .catch((error) => {
+          callback(error, null);
+        })
     };
 
     pactPortSeries.push(asyncFunction);
 
   }
+
   // make sure ports are resolved in order
   async.series(pactPortSeries,
     (err, ports) => {
-
       if (err) {
         logger.error(err);
-        process.exit(exitCode);
+        process.exit();
       }
-
       for (let i = 0; i < skyPagesConfig.skyux.pacts.length; i++) {
 
-        let serverHost = (skyPagesConfig.skyux.pacts[i].host || 'localhost');
-        let serverPort = (skyPagesConfig.skyux.pacts[i].port || ports[i]);
-        // saving pact server information so it can carry over into karma config
-        pactServers
-          .savePactServer(skyPagesConfig.skyux.pacts[i].provider, serverHost, serverPort);
-      }
+            let serverHost = (skyPagesConfig.skyux.pacts[i].host || 'localhost');
+            let serverPort = (skyPagesConfig.skyux.pacts[i].port || ports[i]);
+            // saving pact server information so it can carry over into karma config
+            pactServers
+              .savePactServer(skyPagesConfig.skyux.pacts[i].provider, serverHost, serverPort);
+          }
 
-      let proxy = httpProxy.createProxyServer({});
+          let proxy = httpProxy.createProxyServer({});
 
-      // proxy requests to pact server to contain actual host url rather than the karma url
-      proxy.on('proxyReq', function (proxyReq) {
-        let origin = skyPagesConfig.skyux.host.url || 'https://host.nxt.blackbaud.com';
-        proxyReq.setHeader('Origin', origin);
-      });
-      // revert CORS header value back to karma url so that requests are successful
-      proxy.on('proxyRes', function (proxyRes, req) {
-        proxyRes.headers['Access-Control-Allow-Origin'] = req.headers.origin;
-      });
-
-      http.createServer((req, res) => {
-
-        // provider is part of path so that consuming app can make requests to multiple pact
-        // servers from one proxy server.  Use that value to identify proper pact server and then
-        // remove from request url.
-        let provider = url.parse(req.url).pathname.split('/')[1];
-        req.url = req.url.split(provider)[1];
-        if (Object.keys(pactServers.getAllPactServers()).indexOf(provider) !== -1) {
-          proxy.web(req, res, {
-            target: pactServers.getPactServer(provider).fullUrl
+          // proxy requests to pact server to contain actual host url rather than the karma url
+          proxy.on('proxyReq', function (proxyReq) {
+            let origin;
+            origin = (skyPagesConfig.skyux.host || {}).url || 'https://host.nxt.blackbaud.com';
+            proxyReq.setHeader('Origin', origin);
           });
-        } else {
-          logger
-            .error(`Pact proxy path is invalid.  Expected format is base/provider-name/api-path.`);
-        }
-      })
-      .on('connect', () => {
-        logger
-          .log(
-          `Pact proxy server successfully started on http://localhost:${ports[ports.length - 1]}`
-          );
-      })
-      .listen(ports[ports.length - 1], 'localhost');
+          // revert CORS header value back to karma url so that requests are successful
+          proxy.on('proxyRes', function (proxyRes, req) {
+            proxyRes.headers['Access-Control-Allow-Origin'] = req.headers.origin;
+          });
 
-      // for use by consuming app
-      pactServers.savePactProxyServer(`http://localhost:${ports[ports.length - 1]}`);
+          http.createServer((req, res) => {
 
-      const karmaConfigUtil = require('karma').config;
-      const karmaConfigPath = skyPagesConfigUtil.outPath(`config/karma/${command}.karma.conf.js`);
-      const karmaConfig = karmaConfigUtil.parseConfig(karmaConfigPath);
+            // provider is part of path so that consuming app can make requests to multiple pact
+            // servers from one proxy server.  Use that value to identify proper pact server and then
+            // remove from request url.
+            let provider = url.parse(req.url).pathname.split('/')[1];
+            req.url = req.url.split(provider)[1];
+            if (Object.keys(pactServers.getAllPactServers()).indexOf(provider) !== -1) {
+              proxy.web(req, res, {
+                target: pactServers.getPactServer(provider).fullUrl
+              });
+            } else {
+              logger
+                .error(`Pact proxy path is invalid.  Expected format is base/provider-name/api-path.`);
+            }
+          })
+            .on('connect', () => {
+              logger
+                .info(
+                `Pact proxy server successfully started on http://localhost:${ports[ports.length - 1]}`
+                );
+            })
+            .listen(ports[ports.length - 1], 'localhost');
 
-      const server = new Server(karmaConfig, onExit);
-      server.on('run_start', onRunStart);
-      server.on('run_complete', onRunComplete);
-      server.start();
+          // for use by consuming app
+          pactServers.savePactProxyServer(`http://localhost:${ports[ports.length - 1]}`);
+
+          const karmaConfigUtil = karma.config;
+          const karmaConfigPath = configResolver.resolve(command, argv);
+          const karmaConfig = karmaConfigUtil.parseConfig(karmaConfigPath);
+
+          let lintResult;
+
+          const onRunStart = () => {
+            lintResult = tsLinter.lintSync();
+          };
+
+          const onRunComplete = () => {
+            if (lintResult && lintResult.exitCode > 0) {
+              // Pull the logger out of the execution stream to let it print
+              // after karma's coverage reporter.
+              setTimeout(() => {
+                logger.error('Process failed due to linting errors:');
+                lintResult.errors.forEach(error => logger.error(error));
+              }, 10);
+            }
+          };
+
+          const onExit = (exitCode) => {
+            if (exitCode === 0) {
+              if (lintResult) {
+                exitCode = lintResult.exitCode;
+              }
+            }
+
+            logger.info(`Karma has exited with ${exitCode}.`);
+            process.exit(exitCode);
+          };
+
+          const server = new Server(karmaConfig, onExit);
+          server.on('run_start', onRunStart);
+          server.on('run_complete', onRunComplete);
+          server.start();
     });
+
 }
 
 module.exports = pact;
--- test/cli-pact.spec.js
+++ test/cli-pact.spec.js
@@ -3,20 +3,54 @@
 
 const mock = require('mock-require');
 const logger = require('../utils/logger');
+const portfinder = require('portfinder');
 
-describe('cli test', () => {
+fdescribe('cli pact', () => {
   let originalArgv = process.argv;
 
   function MockServer() { }
 
-  MockServer.prototype.on = function () {};
-  MockServer.prototype.start = function () {};
+  MockServer.prototype.on = function () { };
+  MockServer.prototype.start = function () { };
 
   beforeEach(() => {
     spyOn(global, 'setTimeout').and.callFake(cb => cb());
     spyOn(process, 'exit').and.returnValue();
     spyOn(logger, 'info').and.returnValue();
     spyOn(logger, 'error').and.returnValue();
+
+    mock('http-proxy', {
+      createProxyServer: (options) => {
+        return {
+          on: (event, callback) => {
+
+          },
+          web: (req, res, options) => {
+
+          }
+        }
+      }
+    });
+
+    mock('http', {
+      createServer: (callback) => {
+        return {
+          on: (event, callback) => {
+            return {
+              listen: (port, host) => {
+
+              }
+            }
+          }
+        }
+      }
+    });
+
+    mock('portfinder', {
+      getPortPromise: (config) => {
+        return Promise.resolve(config.port);
+      }
+    });
 
     mock('../cli/utils/ts-linter', {
       lintSync: () => {
@@ -28,12 +62,26 @@ describe('cli test', () => {
     });
 
     mock('../config/sky-pages/sky-pages.config', {
-      outPath: (path) => path
+      outPath: (path) => path,
+      getSkyPagesConfig: (path) => {
+        return {
+          skyux: {
+            pacts: [
+              {
+                "provider": "test-provider",
+                "consumer": "test-consumer",
+                "spec": 1
+              }
+            ]
+          }
+        }
+      }
     });
 
     mock('../cli/utils/config-resolver', {
       resolve: (command) => `${command}-config.js`
     });
+
   });
 
   afterEach(() => {
@@ -41,63 +89,61 @@ describe('cli test', () => {
     process.argv = originalArgv;
   });
 
-  it('should load the test config when running test command', () => {
-    let _configPath;
+  it('should load the pact config when running pact command', (done) => {
+    var _configPath;
+    spyOn(MockServer.prototype, 'start').and.callFake(() => {
+      expect(_configPath).toEqual('pact-config.js');
+      done();
+    });
     mock('karma', {
       config: {
         parseConfig: (configPath) => _configPath = configPath
+
       },
       Server: MockServer
     });
-    const test = mock.reRequire('../cli/test');
-    test('test', {});
-    expect(_configPath).toEqual('test-config.js');
+
+    const test = mock.reRequire('../cli/pact');
+    test('pact');
   });
 
-  it('should load the watch config when running watch command', () => {
-    let _configPath;
+  it('should save the current command to argv', (done) => {
+    spyOn(MockServer.prototype, 'start').and.callFake(() => {
+      expect(argv.command).toEqual('pact');
+      done();
+    });
     mock('karma', {
       config: {
-        parseConfig: (configPath) => _configPath = configPath
+        parseConfig: () => { }
       },
       Server: MockServer
     });
-    const test = mock.reRequire('../cli/test');
-    test('watch');
-    expect(_configPath).toEqual('watch-config.js');
-  });
-
-  it('should save the current command to argv', () => {
-    mock('karma', {
-      config: {
-        parseConfig: () => {}
-      },
-      Server: MockServer
-    });
-    const test = mock.reRequire('../cli/test');
+    const test = mock.reRequire('../cli/pact');
     const argv = {};
-    test('watch', argv);
-    expect(argv.command).toEqual('watch');
+    test('pact', argv);
   });
 
-  it('should start a karma server', () => {
-    spyOn(MockServer.prototype, 'start').and.returnValue();
+  it('should start a karma server', (done) => {
+    spyOn(MockServer.prototype, 'start').and.callFake(() => {
+      expect(MockServer.prototype.start).toHaveBeenCalled();
+      done();
+    });
     mock('karma', {
       config: {
-        parseConfig: () => {}
+        parseConfig: () => { }
       },
       Server: MockServer
     });
-    const test = mock.reRequire('../cli/test');
-    test('test');
-    expect(MockServer.prototype.start).toHaveBeenCalled();
+    const test = mock.reRequire('../cli/pact');
+    test('pact');
   });
 
-  it('should process the exit code from karma server', () => {
+  it('should process the exit code from karma server', (done) => {
     let _onExit;
+
     mock('karma', {
       config: {
-        parseConfig: () => {}
+        parseConfig: () => { }
       },
       Server: function (config, onExit) {
         this.on = (hook, callback) => {
@@ -108,35 +154,41 @@ describe('cli test', () => {
             callback();
           }
         };
-        this.start = () => {};
+        this.start = () => {
+          _onExit();
+          expect(process.exit).toHaveBeenCalledWith(0);
+          done();
+        };
       }
     });
-    const test = mock.reRequire('../cli/test');
-    test('test');
-    _onExit();
-    expect(process.exit).toHaveBeenCalledWith(0);
+    const test = mock.reRequire('../cli/pact');
+    test('pact');
+
   });
 
-  it('should not change the exit code if the server has already failed', () => {
+  it('should not change the exit code if the server has already failed', (done) => {
     mock('karma', {
       config: {
-        parseConfig: () => {}
+        parseConfig: () => { }
       },
       Server: function (config, callback) {
         callback(1);
-        this.on = () => {};
-        this.start = () => {};
+        this.on = () => { };
+        this.start = () => {
+          expect(process.exit).toHaveBeenCalledWith(1);
+          done();
+        };
       }
     });
-    const test = mock.reRequire('../cli/test');
-    test('test');
-    expect(process.exit).toHaveBeenCalledWith(1);
+    const test = mock.reRequire('../cli/pact');
+    test('pact');
   });
 
-  it('should execute tslint before each karma run and pass the exit code', () => {
+  it('should execute tslint before each karma run and pass the exit code', (done) => {
     let _hooks = [];
     let _exitCode;
     let _onExit;
+
     mock.stop('../cli/utils/ts-linter');
     mock('../cli/utils/ts-linter', {
       lintSync: () => {
@@ -149,7 +201,7 @@ describe('cli test', () => {
     });
     mock('karma', {
       config: {
-        parseConfig: () => {}
+        parseConfig: () => { }
       },
       Server: function (config, onExit) {
         _onExit = onExit;
@@ -157,18 +209,20 @@ describe('cli test', () => {
           _hooks.push(hook);
           callback();
         };
-        this.start = () => {};
+        this.start = () => {
+          _onExit(0);
+          expect(_hooks[0]).toEqual('run_start');
+          expect(_exitCode).toEqual(1);
+          expect(process.exit).toHaveBeenCalledWith(1);
+          done();
+        };
       }
     });
-    const test = mock.reRequire('../cli/test');
-    test('test');
-    _onExit(0);
-    expect(_hooks[0]).toEqual('run_start');
-    expect(_exitCode).toEqual(1);
-    expect(process.exit).toHaveBeenCalledWith(1);
+    const test = mock.reRequire('../cli/pact');
+    test('pact');
   });
 
-  it('should not output a tslint error message if tslint passes', () => {
+  it('should not output a tslint error message if tslint passes', (done) => {
     let _hooks = [];
     let _onExit;
     mock.stop('../cli/utils/ts-linter');
@@ -181,7 +235,7 @@ describe('cli test', () => {
     });
     mock('karma', {
       config: {
-        parseConfig: () => {}
+        parseConfig: () => { }
       },
       Server: function (config, onExit) {
         _onExit = onExit;
@@ -189,17 +243,19 @@ describe('cli test', () => {
           _hooks.push(hook);
           callback();
         };
-        this.start = () => {};
+        this.start = () => {
+          _onExit(0);
+          expect(_hooks[1]).toEqual('run_complete');
+          expect(process.exit).toHaveBeenCalledWith(0);
+          done();
+        };
       }
     });
-    const test = mock.reRequire('../cli/test');
-    test('test');
-    _onExit(0);
-    expect(_hooks[1]).toEqual('run_complete');
-    expect(process.exit).toHaveBeenCalledWith(0);
+    const test = mock.reRequire('../cli/pact');
+    test('pact');
   });
 
-  it('should handle signal interrupted', () => {
+  it('should handle signal interrupted', (done) => {
     let _onExit;
 
     mock.stop('../cli/utils/ts-linter');
@@ -211,20 +267,383 @@ describe('cli test', () => {
 
     mock('karma', {
       config: {
-        parseConfig: () => {}
+        parseConfig: () => { }
       },
       Server: function (config, onExit) {
         _onExit = onExit;
         this.on = (hook, callback) => callback();
-        this.start = () => {};
+        this.start = () => {
+          _onExit(0);
+          expect(process.exit).toHaveBeenCalledWith(0);
+          done();
+        };
       }
     });
 
-    const test = mock.reRequire('../cli/test');
+    const test = mock.reRequire('../cli/pact');
 
-    test('test');
-    _onExit(0);
+    test('pact');
+  });
 
-    expect(process.exit).toHaveBeenCalledWith(0);
+  it('handles error in port finding and exits process', (done) => {
+
+    mock('karma', {
+      config: {
+        parseConfig: () => { }
+      },
+      Server: MockServer
+    });
+
+    mock.stop('portfinder');
+    mock('portfinder', {
+      getPortPromise: (config) => {
+        return Promise.reject('error in finding available port');
+      }
+    });
+
+    // round about way of checking these calls after process.exit() should cancel the process.
+    // Since it's spied on it continues execution so synthetically cancel it by calling done()
+    // in the next called function.
+    mock('../utils/pact-servers', {
+      savePactServer: (a, b, c) => {
+        expect(logger.error)
+          .toHaveBeenCalledWith('error in finding available port');
+        expect(process.exit).toHaveBeenCalled();
+        done();
+      },
+      savePactProxyServer: (url) => {
+
+      }
+    });
+
+    const test = mock.reRequire('../cli/pact');
+
+    // process.exit is spyed on, so command continues execution after it should exit.
+    // Need to eat the error to reach the expectations
+    test('pact');
+
+  });
+
+  it('exits process if pacts are not provided in config', (done) => {
+    mock('karma', {
+      config: {
+        parseConfig: () => { }
+      },
+      Server: MockServer
+    });
+
+    mock.stop('../config/sky-pages/sky-pages.config');
+    mock('../config/sky-pages/sky-pages.config', {
+      outPath: (path) => path,
+      getSkyPagesConfig: (path) => {
+        return {
+          skyux: {
+
+          }
+        }
+      }
+    });
+    const test = mock.reRequire('../cli/pact');
+
+    // process.exit is spyed on, so command continues execution after it should exit.
+    // Need to eat the error to reach the expectations
+    try {
+      test('pact');
+    }
+    catch (exception) {
+      expect(logger.error)
+        .toHaveBeenCalledWith('skyux pact failed! pacts does not exist on configuration file.');
+      expect(process.exit).toHaveBeenCalled();
+      done();
+    }
+
+  });
+
+  it('sets headers upon request and response on proxy server', (done) => {
+
+    mock('karma', {
+      config: {
+        parseConfig: () => { }
+      },
+      Server: MockServer
+    });
+
+
+    let originHeader;
+    let proxyReq = {
+      setHeader: (header, value) => {
+        originHeader = value;
+      }
+    }
+    let proxyRes = {
+      headers: {
+
+      }
+    };
+    let response = {
+      headers: {
+        origin: 'localhost:1234'
+      }
+    }
+    mock('http-proxy', {
+      createProxyServer: (options) => {
+        return {
+          on: (event, callback) => {
+            if (event === 'proxyReq')
+              callback(proxyReq);
+            else
+              callback(proxyRes, response);
+          },
+          web: (req, res, options) => {
+
+          }
+        }
+      }
+    });
+    const test = mock.reRequire('../cli/pact');
+
+    spyOn(MockServer.prototype, 'start').and.callFake(() => {
+      expect(originHeader).toEqual('https://host.nxt.blackbaud.com');
+      proxyRes.headers['Access-Control-Allow-Origin'] = response.headers.origin;
+      expect(proxyRes.headers['Access-Control-Allow-Origin']).toEqual(response.headers.origin);
+      done();
+    });
+
+    test('pact');
+
+  });
+
+  it('sets Origin header on request to config setting if it exists', (done) => {
+    mock('karma', {
+      config: {
+        parseConfig: () => { }
+      },
+      Server: MockServer
+    });
+
+    mock.stop('../config/sky-pages/sky-pages.config');
+    mock('../config/sky-pages/sky-pages.config', {
+      outPath: (path) => path,
+      getSkyPagesConfig: (path) => {
+        return {
+          skyux: {
+            pacts: [
+              {
+                "provider": "test-provider",
+                "consumer": "test-consumer",
+                "spec": 1
+              }
+            ],
+            host: {
+              url: 'test.nxt.blackbaud.com'
+            }
+          }
+        }
+      }
+    });
+
+    let originHeader;
+    let proxyReq = {
+      setHeader: (header, value) => {
+        originHeader = value;
+      }
+    }
+    let proxyRes = {
+      headers: {
+
+      }
+    };
+    let response = {
+      headers: {
+        origin: 'localhost:1234'
+      }
+    }
+    mock('http-proxy', {
+      createProxyServer: (options) => {
+        return {
+          on: (event, callback) => {
+            if (event === 'proxyReq')
+              callback(proxyReq);
+            else
+              callback(proxyRes, response);
+          },
+          web: (req, res, options) => {
+
+          }
+        }
+      }
+    });
+    const test = mock.reRequire('../cli/pact');
+
+    spyOn(MockServer.prototype, 'start').and.callFake(() => {
+      expect(originHeader).toEqual('test.nxt.blackbaud.com');
+      proxyRes.headers['Access-Control-Allow-Origin'] = response.headers.origin;
+      expect(proxyRes.headers['Access-Control-Allow-Origin']).toEqual(response.headers.origin);
+      done();
+    });
+    test('pact');
+
+  });
+
+  it('gets correct pact server before call to proxy server', (done) => {
+    mock('karma', {
+      config: {
+        parseConfig: () => { }
+      },
+      Server: MockServer
+    });
+
+    let request = {
+      url: 'http://localhost:9876/test-provider/api/test'
+    }
+    mock.stop('http');
+    mock('http', {
+      createServer: (callback) => {
+        callback(request, {});
+        return {
+          on: (event, callback) => {
+            return {
+              listen: (port, host) => {
+
+              }
+            }
+          }
+        }
+      }
+    });
+
+    let targetUrl;
+    mock.stop('http-proxy');
+    mock('http-proxy', {
+      createProxyServer: (options) => {
+        return {
+          on: (event, callback) => {
+
+          },
+          web: (req, res, options) => {
+            targetUrl = options.target;
+          }
+        }
+      }
+    });
+
+    const test = mock.reRequire('../cli/pact');
+
+    spyOn(MockServer.prototype, 'start').and.callFake(() => {
+
+      expect(targetUrl).toEqual('http://localhost:8000');
+      done();
+    });
+    test('pact');
+
+  });
+
+  it('logs error when malformed proxy url is requested', (done) => {
+    mock('karma', {
+      config: {
+        parseConfig: () => { }
+      },
+      Server: MockServer
+    });
+
+    let request = {
+      url: 'http://localhost:9876/test-provdr/api/test'
+    }
+    mock.stop('http');
+    mock('http', {
+      createServer: (callback) => {
+        callback(request, {});
+        return {
+          on: (event, callback) => {
+            return {
+              listen: (port, host) => {
+
+              }
+            }
+          }
+        }
+      }
+    });
+
+    let targetUrl;
+    mock.stop('http-proxy');
+    mock('http-proxy', {
+      createProxyServer: (options) => {
+        return {
+          on: (event, callback) => {
+
+          },
+          web: (req, res, options) => {
+            targetUrl = options.target;
+          }
+        }
+      }
+    });
+
+    const test = mock.reRequire('../cli/pact');
+
+    spyOn(MockServer.prototype, 'start').and.callFake(() => {
+      expect(logger.error)
+        .toHaveBeenCalledWith(`Pact proxy path is invalid.  Expected format is base/provider-name/api-path.`);
+      done();
+    });
+    test('pact');
+
+  });
+
+  it('logs when proxy server is successfully started', (done) => {
+    mock('karma', {
+      config: {
+        parseConfig: () => { }
+      },
+      Server: MockServer
+    });
+
+    let request = {
+      url: 'http://localhost:9876/test-provdr/api/test'
+    }
+    mock.stop('http');
+    mock('http', {
+      createServer: (callback) => {
+        callback(request, {});
+        return {
+          on: (event, callback) => {
+            if (event === 'connect') {
+              callback();
+            }
+            return {
+              listen: (port, host) => {
+
+              }
+            }
+          }
+        }
+      }
+    });
+
+    let targetUrl;
+    mock.stop('http-proxy');
+    mock('http-proxy', {
+      createProxyServer: (options) => {
+        return {
+          on: (event, callback) => {
+
+          },
+          web: (req, res, options) => {
+            targetUrl = options.target;
+          }
+        }
+      }
+    });
+
+    const test = mock.reRequire('../cli/pact');
+
+    spyOn(MockServer.prototype, 'start').and.callFake(() => {
+      expect(logger.info)
+        .toHaveBeenCalledWith(`Pact proxy server successfully started on http://localhost:8001`);
+      done();
+    });
+    test('pact');
+
   });
 });
