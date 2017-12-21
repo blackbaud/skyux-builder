51367074c1d05647ca79661a310bb7e05203b5e4
Josh Landi <Josh.Landi@blackbaud.me>
2017-12-21 10:37:56 -0500

    adding necessary dependencies for a working model.  Updating service to include matchers and adding new property to pact object to allow control over write mode.

--- config/karma/pact.karma.conf.js
+++ config/karma/pact.karma.conf.js
@@ -28,7 +28,8 @@ function getConfig(config) {
       pact.dir = pact.dir || path.resolve(process.cwd(), 'pacts');
       pact.host = pactServers.getPactServer(pact.provider).host;
       pact.port = pactServers.getPactServer(pact.provider).port;
-      pact.pactFileWriteMode = 'merge';
+      pact.pactFileWriteMode = pact.pactFileWriteMode || 'overwrite';
+
       i++;
     });
   } else {
@@ -37,7 +38,7 @@ function getConfig(config) {
 
   config.set({
     frameworks: config.frameworks.concat('pact'),
-    files: config.files.concat(path.resolve(process.cwd(), 'node_modules/pact/src', `pact-web.js`)),
+    files: config.files.concat(path.resolve(process.cwd(), 'node_modules/pact-web', `pact-web.js`)),
     pact: skyPagesConfig.skyux.pacts,
     plugins: config.plugins.concat('@pact-foundation/karma-pact'),
     webpack: testWebpackConfig.getWebpackConfig(skyPagesConfig, argv)
