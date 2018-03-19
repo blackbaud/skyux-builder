77ff1ab65e6c8ef33e6ad9f7762b05ce37766404
Bobby Earl <bobby.earl@blackbaud.com>
2018-03-19 15:29:13 -0400

    Using logger.logColor for Karma and Protractor

--- config/protractor/protractor.conf.js
+++ config/protractor/protractor.conf.js
@@ -3,6 +3,7 @@
 
 const path = require('path');
 const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+const logger = require('@blackbaud/skyux-logger');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -24,7 +25,7 @@ exports.config = {
   // seleniumAddress: 'http://localhost:4444/wd/hub',
   framework: 'jasmine',
   jasmineNodeOpts: {
-    showColors: true,
+    showColors: logger.logColor,
     defaultTimeoutInterval: 30000
   },
   useAllAngular2AppRoots: true,
