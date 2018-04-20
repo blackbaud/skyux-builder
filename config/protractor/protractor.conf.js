b9fc84b44d19694438ed558fa5c9345ec3bf4e78
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-04-20 12:58:18 -0400

    Reverted protractor config

--- config/protractor/protractor.conf.js
+++ config/protractor/protractor.conf.js
@@ -2,24 +2,27 @@
 'use strict';
 
 const path = require('path');
-const { SpecReporter } = require('jasmine-spec-reporter');
+const SpecReporter = require('jasmine-spec-reporter').SpecReporter;
 const logger = require('@blackbaud/skyux-logger');
 
-const config = {
+exports.config = {
   allScriptsTimeout: 11000,
   specs: [
-    path.join(process.cwd(), 'e2e', '**', '*.e2e-spec.ts')
+    path.join(
+      process.cwd(),
+      'e2e',
+      '**',
+      '*.e2e-spec.ts'
+    )
   ],
   capabilities: {
     'browserName': 'chrome',
     'chromeOptions': {
-      'args': [
-        '--disable-extensions',
-        '--ignore-certificate-errors'
-      ]
+      'args': ['--disable-extensions --ignore-certificate-errors']
     }
   },
   directConnect: true,
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: logger.logColor,
@@ -33,8 +36,4 @@ const config = {
   onPrepare: function () {
     jasmine.getEnv().addReporter(new SpecReporter());
   }
-};
-
-module.exports = {
-  config
 };
