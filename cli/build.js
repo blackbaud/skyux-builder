b72b12b9456e8d45489b0a16a23723abdcdf228a
Bobby Earl <bobby.earl@blackbaud.com>
2018-05-02 10:06:52 -0400

    Fixed linting error

--- cli/build.js
+++ cli/build.js
@@ -153,7 +153,7 @@ function buildCompiler(argv, skyPagesConfig, webpack, isAot) {
   assetsProcessor.setSkyAssetsLoaderUrl(config, skyPagesConfig, assetsBaseUrl, assetsRel);
 
   return runCompiler(webpack, config, isAot)
-    .then((stats) => {
+    .then(() => {
       if (isAot) {
         cleanupAot();
       }
