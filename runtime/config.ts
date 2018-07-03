5fda84287bdf0da6ddcc777fd0e3caa722504b7e
Bobby Earl <bobby.earl@blackbaud.com>
2018-07-03 13:00:03 -0400

    Allowing device property

--- runtime/config.ts
+++ runtime/config.ts
@@ -24,6 +24,7 @@ export interface SkyuxConfigBrowserTestSettings {
   osVersion?: string;
   browser?: string;
   browserVersion?: string;
+  device?: string;
 }
 
 export interface SkyuxConfigE2eTestSettings {
