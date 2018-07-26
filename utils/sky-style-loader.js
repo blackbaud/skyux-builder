b931678573d98708b6f76000ffc2b83455a2494d
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-07-26 10:47:35 -0400

    Updated style loader

--- utils/sky-style-loader.js
+++ utils/sky-style-loader.js
@@ -6,29 +6,24 @@ var FontFaceObserver = require('fontfaceobserver');
 
 var stylesAreLoaded = false;
 
-var LOAD_TIMEOUT = 30000;
+// Set to a number below Jasmine's default of 5000
+// so that tests can still run if the font's fail to load.
+const LOAD_TIMEOUT = 4000;
 
 module.exports = {
   loadStyles: function () {
-    var fontAwesome = new FontFaceObserver('FontAwesome');
-    var blackbaudSans = new FontFaceObserver('Blackbaud Sans');
-    var promise;
+    const fontAwesome = new FontFaceObserver('FontAwesome');
+    const blackbaudSans = new FontFaceObserver('Blackbaud Sans');
 
-    promise = Promise.all(
-      [
-        // Specify a character for FontAwesome since some browsers will fail to detect
-        // when the font is loaded unless a known character with a different width
-        // than the default is not specified.
-        fontAwesome.load('\uf0fc', LOAD_TIMEOUT),
-        blackbaudSans.load(null, LOAD_TIMEOUT)
-      ]
-    );
-
-    promise.then(function () {
+    return Promise.all([
+      // Specify a character for FontAwesome since some browsers will fail to detect
+      // when the font is loaded unless a known character with a different width
+      // than the default is not specified.
+      fontAwesome.load('\uf0fc', LOAD_TIMEOUT),
+      blackbaudSans.load(null, LOAD_TIMEOUT)
+    ]).then(() => {
       stylesAreLoaded = true;
     });
-
-    return promise;
   },
 
   stylesAreLoaded: function () {
--- utils/spec-styles.js
+++ utils/spec-styles.js
@@ -2,13 +2,17 @@
 
 'use strict';
 
-var styleLoader = require('./sky-style-loader');
+const styleLoader = require('./sky-style-loader');
 
 // A race condition exists in Firefox where tests can begin before styles are loaded.
 // This will ensure that styles are loaded before tests run by ensuring the style rule
 // for the HTML hidden property defined in sky.scss has been applied.
 (function () {
   beforeAll(function (done) {
-    styleLoader.loadStyles().then(done);
+    styleLoader.loadStyles().then(done).catch((err) => {
+      console.warn('[Style Loader Error]', err);
+      // Allow tests to continue running even if fonts fail to load.
+      done();
+    });
   });
 }());
