5914bcc2cf3340ced5cb1654f8ae8ee46232ccb9
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-14 16:19:50 -0400

    Removed auth http

--- runtime/auth-token-provider.ts
+++
@@ -1,9 +0,0 @@
-import { BBAuth, BBAuthGetTokenArgs } from '@blackbaud/auth-client';
-
-export class SkyAuthTokenProvider {
-
-  public getToken(args?: BBAuthGetTokenArgs): Promise<string> {
-    return BBAuth.getToken(args);
-  }
-
-}
--- runtime/index.ts
+++ runtime/index.ts
@@ -1,5 +1,3 @@
-export * from './auth-http';
-export * from './auth-token-provider';
 export * from './directives';
 export * from './window-ref';
 export * from './runtime.module';
