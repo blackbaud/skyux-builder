a99a029c7784e786852d9a88fc649998ea402785
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-08-14 13:21:56 -0400

    Removed pact

--- runtime/index.ts
+++ runtime/index.ts
@@ -1,7 +1,5 @@
 export * from './auth-http';
 export * from './auth-token-provider';
-export * from './pact-auth-token-provider';
-export * from './pact.service';
 export * from './directives';
 export * from './window-ref';
 export * from './runtime.module';
