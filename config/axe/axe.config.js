4ec2c15ced0461abe9ea5d6924790904598920e1
Conor Wright <Conor.Wright@blackbaud.com>
2018-09-05 13:59:43 -0400

    added link to axe issue

--- config/axe/axe.config.js
+++ config/axe/axe.config.js
@@ -50,7 +50,7 @@ const defaults = {
     'bypass': { 'enabled': true },
     'tabindex': { 'enabled': true },
 
-    // TEMP: this should be re-enabled when we upgrade to axe-core ^3.1.1
+    // TODO: this should be re-enabled when we upgrade to axe-core ^3.1.1 (https://github.com/dequelabs/axe-core/issues/961)
     'aria-allowed-attr': { 'enabled': false },
     'aria-required-attr': { 'enabled': true },
     'aria-required-children': { 'enabled': true },
