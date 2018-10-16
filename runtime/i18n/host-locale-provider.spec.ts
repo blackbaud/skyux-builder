51d7bda11d21b612f51ad2794b19ac38e4f57a89
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-10-16 14:33:29 -0400

    Added host locale provider

---
+++ runtime/i18n/host-locale-provider.spec.ts
@@ -0,0 +1,58 @@
+import { Observable } from 'rxjs/Observable';
+
+import {
+  SkyAppHostLocaleProvider
+} from './host-locale-provider';
+
+describe('Host locale provider', () => {
+
+  const mockWindowRef: any = {
+    nativeWindow: {
+      SKYUX_HOST: {
+        acceptLanguage: 'en-GB'
+      }
+    }
+  };
+
+  it('should get locale info from the global SKYUX_HOST variable', (done) => {
+    const localeProvider = new SkyAppHostLocaleProvider(mockWindowRef);
+
+    localeProvider.getLocaleInfo().subscribe((info: any) => {
+      expect(info.locale).toBe('en-GB');
+      done();
+    });
+  });
+
+  it(
+    'should fall back to default local if the global SKYUX_HOST variable does not ' +
+    'specify a language',
+    (done) => {
+      mockWindowRef.nativeWindow.SKYUX_HOST.acceptLanguage = undefined;
+
+      const localeProvider = new SkyAppHostLocaleProvider(mockWindowRef);
+
+      localeProvider.getLocaleInfo().subscribe((info: any) => {
+        expect(info.locale).toBe('en-US');
+        done();
+      });
+    }
+  );
+
+  it('should get locale info from the provided locale provider', (done) => {
+    const localeProvider = new SkyAppHostLocaleProvider(
+      mockWindowRef,
+      {
+        getLocaleInfo: () => {
+          return Observable.of({
+            locale: 'es-MX'
+          });
+        }
+      }
+    );
+
+    localeProvider.getLocaleInfo().subscribe((info: any) => {
+      expect(info.locale).toBe('es-MX');
+      done();
+    });
+  });
+});
--- runtime/i18n/host-locale-provider.ts
+++ runtime/i18n/host-locale-provider.ts
@@ -1,3 +1,54 @@
-export {
-  SkyAppHostLocaleProvider
-} from '@skyux/i18n/modules/i18n/host-locale-provider';
+import {
+  Injectable,
+  Optional
+} from '@angular/core';
+
+import {
+  SkyAppWindowRef
+} from '@skyux/core/modules/window';
+
+import {
+  Observable
+} from 'rxjs/Observable';
+
+import {
+  SkyAppLocaleInfo
+} from './locale-info';
+
+import {
+  SkyAppLocaleProvider
+} from './locale-provider';
+
+@Injectable()
+export class SkyAppHostLocaleProvider extends SkyAppLocaleProvider {
+
+  public static readonly defaultLocale = 'en-US';
+
+  constructor(
+    private windowRef: SkyAppWindowRef,
+    @Optional() private localeProvider?: SkyAppLocaleProvider
+  ) {
+    super();
+  }
+
+  public getLocaleInfo(): Observable<SkyAppLocaleInfo> {
+    if (this.localeProvider) {
+      return this.localeProvider.getLocaleInfo();
+    }
+
+    let locale: string;
+
+    const skyuxHost = (this.windowRef.nativeWindow as any).SKYUX_HOST;
+
+    if (skyuxHost) {
+      const acceptLanguage = skyuxHost.acceptLanguage || '';
+      locale = acceptLanguage.split(',')[0];
+    }
+
+    locale = locale || SkyAppHostLocaleProvider.defaultLocale;
+
+    return Observable.of({
+      locale: locale
+    });
+  }
+}
