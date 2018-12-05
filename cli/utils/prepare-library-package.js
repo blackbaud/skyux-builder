169f4dc83f59414034040e9f2b26cbd94908230f
Blackbaud-SteveBrush <steve.brush@blackbaud.com>
2018-12-05 08:23:56 -0500

    Use separate package.json for libraries

--- cli/utils/prepare-library-package.js
+++ cli/utils/prepare-library-package.js
@@ -2,13 +2,24 @@
 'use strict';
 
 const fs = require('fs-extra');
+const path = require('path');
 const logger = require('@blackbaud/skyux-logger');
 const skyPagesConfigUtil = require('../../config/sky-pages/sky-pages.config');
 
 function makePackageFileForDist() {
-  const contents = fs.readJsonSync(
-    skyPagesConfigUtil.spaPath('package.json')
-  );
+  const packageJsonRelativeDir = path.join('src', 'app', 'public', 'package.json');
+  const packageJsonAbsolutePath = skyPagesConfigUtil.spaPath(packageJsonRelativeDir);
+
+  if (!fs.existsSync(packageJsonAbsolutePath)) {
+    logger.error('SKY UX library failed to compile!');
+    logger.error(
+      `You must provide a package.json file for your library at: "${packageJsonRelativeDir}".`
+    );
+    process.exit(1);
+    return;
+  }
+
+  const contents = fs.readJsonSync(packageJsonAbsolutePath);
   contents.module = 'index.js';
   contents.main = 'bundles/bundle.umd.js';
 
@@ -42,4 +53,6 @@ function copyFilesToDist() {
 module.exports = () => {
   makePackageFileForDist();
   copyFilesToDist();
+
+  logger.info('SKY UX library bundled successfully!');
 };
