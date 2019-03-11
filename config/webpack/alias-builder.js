c44fce315df271b9e75113ac23f69f61359eef3f
Steve Brush <steve.brush@blackbaud.com>
2019-03-11 13:45:41 -0400

    Fixed module aliases for `skyux build` and `skyux e2e` commands. (#519)
    * Fix moduleAliases to work with builds
    
    * Fixed unit tests

--- config/webpack/alias-builder.js
+++ config/webpack/alias-builder.js
@@ -51,8 +51,20 @@ module.exports = {
     // Allow SPAs to provide custom module aliases.
     const moduleAliases = skyPagesConfig.skyux.moduleAliases;
     if (moduleAliases) {
+      const command = skyPagesConfig.runtime.command;
+
       Object.keys(moduleAliases).forEach((key) => {
-        alias[key] = spaPath(moduleAliases[key]);
+        const modulePath = moduleAliases[key];
+
+        switch (command) {
+          case 'build':
+          case 'e2e':
+            alias[key] = skyPagesConfigUtil.spaPathTemp(modulePath);
+            break;
+          default:
+            alias[key] = spaPath(modulePath);
+            break;
+        }
       });
     }
 
--- test/config-webpack-common.spec.js
+++ test/config-webpack-common.spec.js
@@ -186,4 +186,42 @@ describe('config webpack common', () => {
     expect(alias['@skyux/foo']).toBe(path.join(process.cwd(), './src/app/public'));
   });
 
+  it('should allow for custom alias resolution for builds', () => {
+    const lib = mock.reRequire('../config/webpack/common.webpack.config');
+
+    const config = lib.getWebpackConfig({
+      runtime: runtimeUtils.getDefaultRuntime({
+        command: 'build'
+      }),
+      skyux: {
+        moduleAliases: {
+          '@skyux/foo': './src/app/public'
+        }
+      }
+    });
+
+    const alias = config.resolve.alias;
+
+    expect(alias['@skyux/foo']).toBe(path.join(process.cwd(), '.skypagestmp/src/app/public'));
+  });
+
+  it('should allow for custom alias resolution for e2e', () => {
+    const lib = mock.reRequire('../config/webpack/common.webpack.config');
+
+    const config = lib.getWebpackConfig({
+      runtime: runtimeUtils.getDefaultRuntime({
+        command: 'e2e'
+      }),
+      skyux: {
+        moduleAliases: {
+          '@skyux/foo': './src/app/public'
+        }
+      }
+    });
+
+    const alias = config.resolve.alias;
+
+    expect(alias['@skyux/foo']).toBe(path.join(process.cwd(), '.skypagestmp/src/app/public'));
+  });
+
 });
